8936a217fe6c8502647df527e79ceead502da452
Kehad <keahnney01@gmail.com>
2024-08-30 11:37:49 +0100

    figma plugin 7

--- edit.js
+++ edit.js
@@ -2,16 +2,11 @@
 figma.showUI(__html__);
 figma.ui.onmessage = async (msg) => {
     if (msg.type === "replace-text") {
-        //   const {  newText } = msg;
-        console.log("msg");
-        console.log(msg);
-        console.log("msg");
-        await replaceText(msg.newText);
-        9;
+        await replaceText(msg.newText, msg.instanceValue);
+        // 9;
         // await replaceText(oldText, newText);
     }
     else if (msg.type === "replace-image") {
-        const { imageName, newImageUrl } = msg;
         // await replaceImage(imageName, newImageUrl);
     }
 };
@@ -50,137 +45,61 @@ function inspectComponent(node) {
         }
     }
 }
-async function replaceText(newText) {
-    //   const textNodes = figma.currentPage.findAll(
-    //     (node) => node.type === "TEXT" && node.characters === oldText
-    //   );
-    //   for (const node of textNodes) {
-    //     if (node.type === "TEXT") {
-    //       await figma.loadFontAsync(node.fontName as FontName);
-    //       node.characters = newText;
-    //     }
-    //   }
+async function modifyTextNode(textNode, newText) {
+    // Check if the font is available for this text node
+    const fontName = textNode.fontName;
+    // Load the font asynchronously
+    //    await figma.loadFontAsync({ family: "Inter", style: "Regular" })
+    await figma.loadFontAsync(fontName);
+    // Once the font is loaded, you can safely modify the text
+    textNode.characters = newText;
+}
+async function replaceText(newText, instanceValue) {
     const selection = figma.currentPage.selection;
     if (selection.length === 0) {
         figma.notify("No nodes selected!");
     }
     else {
         selection.forEach(async (node) => {
-            // Assume we have a selected ComponentNode or InstanceNode
-            //   const componentToDuplicate = figma.currentPage
-            //     .selection[0] as ComponentNode;
-            const componentToDuplicate = node;
-            if (componentToDuplicate) {
-                // The number of instances you want to create
-                const numberOfInstances = 3;
-                // Array of content to assign to the instances (could be user inputs)
-                const instanceContents = [
-                    {
-                        textContent: "Instance 1",
-                        imageContent: "https://example.com/image1.png",
-                    },
-                    {
-                        textContent: "Instance 2",
-                        imageContent: "https://example.com/image2.png",
-                    },
-                    {
-                        textContent: "Instance 3",
-                        imageContent: "https://example.com/image3.png",
-                    },
-                ];
-                for (let i = 0; i < numberOfInstances; i++) {
-                    // Create an instance of the component
-                    const newInstance = await createComponentInstance(node.id, 100, 100);
-                    // Set the position of each instance (arrange them nicely)
-                    //   newInstance.x =
-                    //     componentToDuplicate.x + i * (componentToDuplicate.width + 20);
-                    //   newInstance.y = componentToDuplicate.y;
-                    // Find text and image layers in the instance to modify them
-                    const textNode = newInstance === null || newInstance === void 0 ? void 0 : newInstance.findOne((node) => node.type === "TEXT");
-                    //   const imageNode = newInstance.findOne(
-                    //     (node) => node.type === "RECTANGLE"
-                    //   ) as RectangleNode;
-                    // Modify the text content of the instance
-                    if (textNode && instanceContents[i].textContent) {
-                        textNode.characters = instanceContents[i].textContent;
+            if (node && (node.type === "COMPONENT" || node.type === "INSTANCE")) {
+                figma.notify("A component is selected!");
+                const componentId = node.id;
+                for (let i = 0; i < instanceValue; i++) {
+                    const newInstance = await createComponentInstance(componentId, 100, 100);
+                    if (newInstance) {
+                        inspectComponent(newInstance); // If you want to inspect the newly created instance
                     }
-                    // Modify the image content of the instance
-                    //             if (imageNode && instanceContents[i].imageContent) {
-                    // const imageHash = await figma.loadImage(instanceContents[i].imageContent);
-                    // imageNode.fills = [
-                    //   { type: "IMAGE", imageHash: imageHash.hash, scaleMode: "FILL" },
-                    // ];
-                    //   const imageHash = figma.createImageFromUrl(
-                    //     instanceContents[i].imageContent
-                    //   );
-                    //   imageNode.fills = [
-                    //     { type: "IMAGE", imageHash, scaleMode: "FILL" },
-                    //   ];
-                    // }
-                    // Append the instance to the current page
-                    figma.currentPage.appendChild(newInstance);
+                    newInstance === null || newInstance === void 0 ? void 0 : newInstance.children.forEach(async (child, index) => {
+                        console.log(`Child ${index + 1}: ${child.name} (Type: ${child.type})`);
+                        if (child.type === "TEXT") {
+                            modifyTextNode(child, newText);
+                        }
+                    });
                 }
-                // Optionally, zoom into the newly created instances
-                figma.viewport.scrollAndZoomIntoView(figma.currentPage.selection);
             }
             else {
-                figma.notify("Please select a component to duplicate.");
+                figma.notify("Please select a component or instance.");
             }
-            //   if (node && (node.type === "COMPONENT" || node.type === "INSTANCE")) {
-            //     figma.notify("A component is selected!");
-            //     console.log(node, node.type, node.id);
-            //     const componentId = node.id;
-            //     const newInstance = await createComponentInstance(
-            //       componentId,
-            //       100,
-            //       100
-            //     );
-            //     console.log("node");
-            //     if (newInstance) {
-            //       // If you want to inspect the newly created instance
-            //       inspectComponent(newInstance);
-            //     }
-            //     console.log("node");
-            //     newInstance?.children.forEach((child, index) => {
-            //       console.log(
-            //         `Child ${index + 1}: ${child.name} (Type: ${child.type})`
-            //       );
-            //       if (child.type === "TEXT") {
-            //         await figma.loadFontAsync()
-            //         console.log(newText);
-            //         console.log(`child characters ${child.characters} `);
-            //         console.log(`child name ${child.name} `);
-            //         child.characters = 'I am a boy';
-            //           console.log(newInstance);
-            //       }
-            //     });
-            //     // Do something with the selected component
-            //   } else {
-            //     figma.notify("Please select a component or instance.");
-            //     console.log(node, node.type);
-            //   }
         });
     }
-    //   figma.notify(`Replaced ${textNodes.length} text elements`);
 }
-// async function replaceImage(imageName: string, newImageUrl: string) {
-//   const imageNodes = figma.currentPage.findAll(
-//     (node) => node.type === "RECTANGLE" && node.name === imageName
-//   );
-//   try {
-//     const image = await figma.createImage();
-//     for (const node of imageNodes) {
-//       if (node.type === "RECTANGLE") {
-//         node.fills = [
-//           { type: "IMAGE", scaleMode: "FILL", imageHash: image.hash },
-//         ];
-//       }
-//     }
-//     figma.notify(`Replaced ${imageNodes.length} image elements`);
-//   } catch (error) {
-//     figma.notify("Error loading image: " + error);
-//   }
-// }
+async function replaceImage(imageName, newImageUrl) {
+    const imageNodes = figma.currentPage.findAll((node) => node.type === "RECTANGLE" && node.name === imageName);
+    try {
+        const image = await figma.createImage();
+        for (const node of imageNodes) {
+            if (node.type === "RECTANGLE") {
+                node.fills = [
+                    { type: "IMAGE", scaleMode: "FILL", imageHash: image.hash },
+                ];
+            }
+        }
+        figma.notify(`Replaced ${imageNodes.length} image elements`);
+    }
+    catch (error) {
+        figma.notify("Error loading image: " + error);
+    }
+}
 // Function to modify children of a component instance
 // function modifyComponentChildren(instance: InstanceNode) {
 //   console.log('Modifying children of instance:', instance.name);
